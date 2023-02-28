2a339fb812e8032662f73258a831ed875bb6ec9a
Adriano Leal <adriano.ramos@sap.com>
2023-02-28 18:53:44 -0300

    exercicio 2 somas sucessivas

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -3,10 +3,23 @@ import java.util.Scanner;
 
 public class Recursion {
 
+    // Exercício I
     public static int multiplicaDois(int x, int y) {
         if (y == 1) { return x; }    
 
         return x + multiplicaDois(x, y-1);
+    }
+
+    // Exercício II  
+    public static int somaSucessiva(int x, int y) { 
+        if (y == 0) { return x; }
+
+        return x + somaSucessiva(x, y-1);
+    }
+
+    // Exercício III - Cálculo de 1 + 1/2 + 1/3 + 1/4 + ... + 1/N.
+    public static double exercicioTres() { 
+        return 0.0;   
     }
 
     public static void main(String[] args) { 
@@ -16,8 +29,10 @@ public class Recursion {
 	    int y = 10;
 
         int result = multiplicaDois(x, y);
+        int resultTwo = somaSucessiva(x, y);
 
 	    System.out.println(result);
+        System.out.println(resultTwo);
     }
 	
 }
