da234911f55b151ceb5f2e3ea4172f9795993049
Adriano Leal <adriano.ramos@sap.com>
2023-02-28 18:19:48 -0300

    Multiplicacao de dois naturais recursiva

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,5 +1,22 @@
 package br.pucrs.alav;
 
 public class Recursion {
+
+    public static int multiplicaDois(int x, int y) {
+        if x == 0 { return 0; }    
+
+        return x + multiplicaDois(x, y-1);
+    }
+
+    public static void main(String[] args) { 
+        Scanner entrada = new Scanner(System.in);
+
+	    int x = 7;
+	    int y = 10;
+        
+        int result = multiplicaDois(x, 7);
+
+	    System.out.println(result);
+    }
 	
 }
